977069ebea49ab25b8671240f31936d769814af0
disco-praanjal <84781900+disco-praanjal@users.noreply.github.com>
2021-08-29 10:34:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+/*--------------------------------------------------------*/
+var PLAY = 1;
+var END = 0;
+var WIN = 2;
+var gameState = PLAY;
+
+var kangaroo, kangaroo_running, kangaroo_collided;
+var jungle, invisiblejungle;
+
+var obstaclesGroup, obstacle1;
+
+var score=0;
+
+var gameOver, restart;
+
+function preload(){
+  kangaroo_running =   loadAnimation("assets/kangaroo1.png","assets/kangaroo2.png","assets/kangaroo3.png");
+  kangaroo_collided = loadAnimation("assets/kangaroo1.png");
+  jungleImage = loadImage("assets/bg.png");
+  shrub1 = loadImage("assets/shrub1.png");
+  shrub2 = loadImage("assets/shrub2.png");
+  shrub3 = loadImage("assets/shrub3.png");
+  obstacle1 = loadImage("assets/stone.png");
+  gameOverImg = loadImage("assets/gameOver.png");
+  restartImg = loadImage("assets/restart.png");
+  jumpSound = loadSound("assets/jump.wav");
+  collidedSound = loadSound("assets/collided.wav");
+}
+
+function setup() {
+  createCanvas(800, 400);
+
+  jungle = createSprite(400,100,400,20);
+  jungle.addImage("jungle",jungleImage);
+  jungle.scale=0.3
+  jungle.x = width /2;
+
+  kangaroo = createSprite(100,300,10,10)
+  kangaroo.addImage("kangaroo", kangaroo_running, kangaroo_collided);
+  kangaroo.scale=0.5
+  kangaroo.colider(circle,300);
+
+  invisiblejungle = createSprite(0,300,800,20)
+  invisiblejungle.visible = false; 
+
+  shrubsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+
+}
+
+function draw() {
+  background(255);
+  
+  kangaroo.x = camera.position.x -270;
+
+  drawSprites();
+
+}
